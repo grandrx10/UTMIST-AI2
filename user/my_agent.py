bb736160edb321ef0683d576d176ee8d5a9c1356
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2025-11-02 00:28:32 -0400

    what?

--- user/my_agent.py
+++ user/my_agent.py
@@ -428,7 +428,7 @@ class SubmittedAgent(Agent):
                 if readible_obs["jumps_left"] == 0 and readible_obs["aerial"]:
                     keys.append('w')
                     keys.append('k')
-            elif random.randint(0, 100) > 95:
+            elif random.randint(0, 100) > 97:
                 self.lastJumped = self.time
                 keys.append('space')
         
